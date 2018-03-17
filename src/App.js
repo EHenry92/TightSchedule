19f8ba80fc729a984f069cd5160e23a7aa0db577
EHenry92 <EHenry692@hotmail.com>
2018-03-17 14:54:41 -0400

    added common components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import firebase from 'firebase';
 import firebaseData from '../secrets';
 import {Text, View} from 'react-native';
+// import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
