59fa177ae1136f0631972e6b37e979cd5ebc51e3
EHenry92 <EHenry692@hotmail.com>
2018-03-31 15:19:41 -0400

    onChangeOrder

--- src/components/SortableList.js
+++ src/components/SortableList.js
@@ -18,48 +18,6 @@ import Row from './SRow'
 const window = Dimensions.get('window');
 
 
-const data = {
-  0: {
-    image: 'https://placekitten.com/200/240',
-    text: 'Chloe',
-  },
-  1: {
-    image: 'https://placekitten.com/200/201',
-    text: 'Jasper',
-  },
-  2: {
-    image: 'https://placekitten.com/200/202',
-    text: 'Pepper',
-  },
-  3: {
-    image: 'https://placekitten.com/200/203',
-    text: 'Oscar',
-  },
-  4: {
-    image: 'https://placekitten.com/200/204',
-    text: 'Dusty',
-  },
-  5: {
-    image: 'https://placekitten.com/200/205',
-    text: 'Spooky',
-  },
-  6: {
-    image: 'https://placekitten.com/200/210',
-    text: 'Kiki',
-  },
-  7: {
-    image: 'https://placekitten.com/200/215',
-    text: 'Smokey',
-  },
-  8: {
-    image: 'https://placekitten.com/200/220',
-    text: 'Gizmo',
-  },
-  9: {
-    image: 'https://placekitten.com/220/239',
-    text: 'Kitty',
-  },
-};
 
 class Basic extends Component {
   componentWillMount() {
@@ -83,7 +41,9 @@ class Basic extends Component {
           style={styles.list}
           contentContainerStyle={styles.contentContainer}
           data={this.props.final}
-          renderRow={this._renderRow} />
+          renderRow={this._renderRow}
+          onChangeOrder = {(newOrder)=> {console.log(newOrder)}}
+          />
       </View>
     );
   }
